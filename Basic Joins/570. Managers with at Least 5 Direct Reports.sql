e67d7c8287b6a8e53ec1f32a4469566a783e36fb
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-03-20 00:24:25 +0530

    joins Solutions

---
+++ Basic Joins/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,43 @@
+/* Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+  
+*/
+
+SELECT e1.name 
+FROM Employee e1
+INNER JOIN Employee e2
+ON e1.id = e2.managerId
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerId) >= 5 
