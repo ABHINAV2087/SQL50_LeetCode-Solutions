a53e62ca2e9ec3e165c678bc0b7854936a529454
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-03-25 22:51:39 +0530

    aggregate  Solutions

--- Basic Aggregate Functions/1251. Average Selling Price.sql
+++ Basic Aggregate Functions/1251. Average Selling Price.sql
@@ -55,8 +55,10 @@ Output:
 +------------+---------------+ */
 
 
-SELECT p.project_id, ROUND(AVG(e.experience_years),2) AS average_years
-FROM Project p 
-LEFT JOIN Employee e
-ON p.employee_id = e.employee_id
-GROUP BY p.project_id
+SELECT p.product_id, 
+       IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u 
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
