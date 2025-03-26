18a97e63a0cb05fd24494c5295adbc9d619a8afc
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-03-26 23:33:26 +0530

    Sorting  Solutions

---
+++ Sorting and Grouping/596. Classes More Than 5 Students.sql
@@ -0,0 +1,45 @@
+/* Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+ */
+
+SELECT class 
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5
