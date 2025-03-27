ffb2717cae407629fcd621d11c796d69d9731a24
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-03-27 22:50:42 +0530

    advanced solutions

---
+++ Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,53 @@
+/* Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+
+For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output: 
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+ */
+
+SELECT 
+    E1.employee_id,
+    E1.name,
+    COUNT(E2.employee_id) AS reports_count,
+    ROUND(AVG(E2.age)) AS average_age
+FROM 
+    Employees E1 INNER JOIN Employees E2
+ON
+    E1.employee_id = E2.reports_to
+GROUP BY
+    E1.employee_id, E1.name
+ORDER BY
+    employee_id
--- Sorting and Grouping/1045. Customers Who Bought All Products.sql
+++ Sorting and Grouping/1045. Customers Who Bought All Products.sql
@@ -1,4 +1,4 @@
-Table: Customer
+/* Table: Customer
 
 +-------------+---------+
 | Column Name | Type    |
@@ -6,10 +6,7 @@ Table: Customer
 | customer_id | int     |
 | product_key | int     |
 +-------------+---------+
-This table may contain duplicates rows. 
-customer_id is not NULL.
-product_key is a foreign key (reference column) to Product table.
- 
+
 
 Table: Product
 
@@ -18,7 +15,7 @@ Table: Product
 +-------------+---------+
 | product_key | int     |
 +-------------+---------+
-product_key is the primary key (column with unique values) for this table.
+
  
 
 Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
@@ -55,4 +52,14 @@ Output:
 +-------------+
 | 1           |
 | 3           |
-+-------------+
++-------------+ */
+
+
+
+SELECT  customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(product_key)
+    FROM Product
+)
